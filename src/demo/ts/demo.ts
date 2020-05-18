47ba9202ff65bf3867db965c2b87bcb8c1d2b1c1
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-05-19 01:02:30 +0200

    refactor(demo): change ui; add event listeners for slider config form's inputs

--- src/demo/ts/demo.ts
+++ src/demo/ts/demo.ts
@@ -2,26 +2,52 @@ import $ from 'jquery';
 import '../styles/style.scss';
 import '../../app/ts/jquery.range';
 
-$('.js-slider-group__target-input').each((_, input) => {
-  const $input = $(input);
-  const $configForm = $input.closest('.js-slider-group').find('.js-slider-config');
+const updateForm = ($form, state): void => {
+  const $textInputs = $form.find('.js-slider-config__input_type_text');
+  $textInputs.each((_, configInput) => {
+    const name = $(configInput).attr('name');
+    $(configInput).val(state[name]);
+  });
+};
 
-  const updateForm = ($form, state): void => {
-    const $inputs = $form.find('.js-slider-config__input_type_text');
-    $inputs.each((__, configInput) => {
-      const name = $(configInput).attr('name');
-      $(configInput).val(state[name]);
+const setEventListeners = ($form, api): void => {
+  const toCamelCase = (s) => s.replace(/([-][a-z])/ig, ($1) => $1.toUpperCase().replace('-', ''));
+  $form.find('.js-slider-config__input_type_text').each((_, textInput) => {
+    $(textInput).on('blur', (event) => {
+      const propName = toCamelCase($(event.target).attr('name'));
+      const propValue = $(event.target).val();
+      api.update({
+        [propName]: propValue,
+      });
     });
-  };
+  });
+  $form.find('.js-slider-config__input_type_checkbox').each((_, checkbox) => {
+    $(checkbox).on('change', (event) => {
+      const propName = toCamelCase($(event.target).attr('name'));
+      const propValue = $(event.target).is(':checked');
+      api.update({
+        [propName]: propValue,
+      });
+    });
+  });
+};
 
-  const rangeSliderAPI = $input.range({
-    onChange(state) {
-      updateForm($configForm, state);
-    },
-    onCreate(state) {
-      updateForm($configForm, state);
-    },
-  }).data('api');
+const init = ($targets): void => {
+  $targets.each((_, target) => {
+    const $target = $(target);
+    const $configForm = $target.closest('.js-slider-group').find('.js-slider-config');
 
-  rangeSliderAPI.update();
-});
+    const sliderApi = $target.range({
+      onChange(state) {
+        updateForm($configForm, state);
+      },
+      onCreate(state) {
+        updateForm($configForm, state);
+      },
+    }).data('api');
+
+    setEventListeners($configForm, sliderApi);
+  });
+};
+
+init($('.js-slider-group__target-input'));
